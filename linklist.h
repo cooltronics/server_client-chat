d7d7666b76cb4538b34e0d79bc56c68f0b911f17
Ratnesh Zinzuwadia <ratnesh_zin@yahoo.com>
2016-04-20 10:49:04 +0530

    Linklist modified

--- linklist.h
+++ linklist.h
@@ -1,5 +1,5 @@
 //uncomment this to make debug ON.
-//#define DEBUG 1
+#define DEBUG 1
 
 #ifdef DEBUG
 #define DBG(fmt, ...)	do { if (DEBUG) fprintf(stdout, "%3d:: IN %s\() :: "fmt" \n",__LINE__,__FUNCTION__, \
